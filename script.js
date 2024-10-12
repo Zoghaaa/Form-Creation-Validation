b5e22b672e89af38288bdbea1ff8af49ec4e8ddf
zogha <fz.bendehbi@gmail.com>
2024-10-12 19:29:45 +0100

    add changes to the file

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
         event.preventDefault()
 
-        const usernameInput = document.getElementById("username").value.trim()
-        const emailInput = document.getElementById("email").value.trim()
-        const passwordInput = document.getElementById("password").value.trim()
+        const username = document.getElementById("username").value.trim()
+        const email = document.getElementById("email").value.trim()
+        const password = document.getElementById("password").value.trim()
 
 ////Initialize Validation Variables:
         let isValid = true
         let messages = []
 
         ////Username Validation:
-        if (usernameInput.length < 3) {
+        if (username.length < 3) {
             isValid = false
             messages.push("Username must be at least 3 characters long.")
         }
 
         ////Email Validation:
 
-        if (!emailInput.includes('@') || !emailInput.includes('.')) {
+        if (!email.includes('@') || !email.includes('.')) {
             isValid = false;
             messages.push("Please enter a valid email address.");
         }
         
         ////Password validation:
-        if(passwordInput.length < 8){
+        if(password.length < 8){
             isValid = false
             messages.push("Password must be at least 8 characters long.");
         }
